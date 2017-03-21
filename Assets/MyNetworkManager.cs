c7694dbd6f789a43ad27b803e38b2cf9de7db29c
Emilijus <slibinas2012@gmail.com>
2017-03-21 21:22:11 +0200

    Lan created

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        Debug.Log("Starting Host");
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Host started");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log("Client start requested");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Client connected to " + conn.address);
+    }
+}
